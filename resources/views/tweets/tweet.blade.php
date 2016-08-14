b95f0aa88652f5837d151fb1f4f825d4f74c8937
arnoldllanes <arn0ldllanes@yahoo.com>
2016-08-13 22:59:49 -0700

    Initial Repo

---
+++ resources/views/tweets/tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="media">
+    <div class="media-left">
+        <img class="img-thumbnail media-object" src="{{ $tweet->user->profile_image_url }}" alt="Avatar">
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">{{ $tweet->user->name }} <span>@</span>{{ $tweet->user->screen_name }} {{ $tweet->created_at }}</h4>
+        <p>{{ $tweet->text }}</p>
+        <p><a target="_blank" href="http://www.twitter.com/{{ $tweet->user->screen_name }}">
+            View on Twitter
+        </a></p>
+    </div>
+</div>
