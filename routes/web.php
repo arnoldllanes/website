45b4e9befe5c78660980149d22b4e47529c74dc1
Eric Van Johnson <eric@diegodev.com>
2016-09-07 23:00:10 -0700

    Stubbing out resources for articles

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,29 @@
 
 Route::get('/', 'HomeController@home');
 
-//Auth
+// Auth
 Auth::routes();
 
-//Pages
+// Pages
 Route::get('home', 'HomeController@index');
 Route::get('about', 'PageController@about');
 Route::get('resources', 'PageController@resources');
 
-//Presentations
+// Articles
+Route::resource('articles', 'ArticleController');
+
+// Presentations
 Route::resource('presentations', 'PresentationController');
 Route::post('presentations/approve/{presentation}', 'PresentationController@approve');
 Route::post('presentations/reject/{presentation}', 'PresentationController@reject');
 
-//Tags
+// Tags
 Route::resource('tags', 'TagController', ['only' => ['index', 'show', 'store']]);
 
-//Presenters
+// Presenters
 Route::resource('presenters', 'PresenterController', ['only' => ['index', 'show']]);
 
-//Comments
+// Comments
 Route::post('comment/{presentationId}', 'CommentController@postComment');
 Route::post('comment/{presentationId}/{commentId}/reply', 'CommentController@postReply');
 Route::get('comment/{commentId}/like', 'CommentController@getLike');
